ee6c5e9bc544ab0c3d6de21da850b4a9bc78ac8c
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-04-09 20:48:33 -0300

    workers files

---
+++ thread/workers/decomposicao.py
@@ -0,0 +1,62 @@
+"""
+Fazer download das imagens dos 100 primeiros pokemons da API
+
+Tempo total (hh:mm:ss.ms) 0:00:11.561982
+
+"""
+
+from threading import Event, Thread
+from queue import Queue
+from time import sleep
+from urllib.parse import urljoin
+from requests import get
+from functions import target, timeit
+
+base_url = 'https://pokeapi.co/api/v2/'
+event = Event()
+queue = Queue(maxsize=101)
+
+
+def get_urls():
+    pokemons = get(urljoin(base_url, 'pokemon/?limit=100')).json()['results']
+    [queue.put(pokemon) for pokemon in pokemons]
+    event.set()
+    queue.put('Kill')
+
+
+class Worker(Thread):
+    def __init__(self, target, queue, *, name='Worker'):
+        super().__init__()
+        self.name = name
+        self.queue = queue
+        self._target = target
+        self._stoped = False
+        print(self.name, 'started.')
+
+    def run(self) -> None:
+        event.wait()
+        while not self.queue.empty():
+            pokemon = self.queue.get()
+            print(self.name, pokemon)
+            if pokemon == 'Kill':
+                self.queue.put(pokemon)
+                self._stoped = True
+                break
+            self._target(pokemon)
+
+    def join(self):
+        while not self._stoped:
+            sleep(0.1)
+
+
+def get_pool(n_th: int):
+    """Retorna um numero n de Threads."""
+    return [Worker(target=target, queue=queue,name=f'Worker_{n}')
+            for n in range(n_th)]
+
+
+with timeit():
+    get_urls()
+    thrs = get_pool(5)
+    [th.start() for th in thrs]
+    [th.join() for th in thrs]
---
+++ thread/workers/functions.py
@@ -0,0 +1,48 @@
+from shutil import copyfileobj, rmtree
+from contextlib import contextmanager
+from datetime import datetime
+from os.path import exists
+from os import makedirs
+from requests import get
+
+path = 'downloads'
+
+if exists(path):
+    rmtree(path)
+makedirs(path)
+
+
+def get_bin_file(args):
+    name, url = args
+    return name, get(url, stream=True).raw
+
+
+def get_sprite_url(url, sprite='front_default'):
+    return url['name'], get(url['url']).json()['sprites'][sprite]
+
+
+def save_file(args, path=path, type='png'):
+    """Salva o binario no disco como imagem"""
+    name, binary = args
+    fname = f'{path}/{name}.{type}'
+    with open(fname, 'wb') as f:
+        copyfileobj(binary, f)
+
+
+def pipeline(*funcs):
+    def inner(argument):
+        state = argument
+        for func in funcs:
+            state = func(state)
+    return inner
+
+
+target = pipeline(get_sprite_url, get_bin_file, save_file)
+
+
+@contextmanager
+def timeit(*args):
+    start_time = datetime.now()
+    yield
+    time_elapsed = datetime.now() - start_time
+    print('Tempo total (hh:mm:ss.ms) {}'.format(time_elapsed))
